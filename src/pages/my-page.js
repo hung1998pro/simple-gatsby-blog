36306434b41efc32d560dd9dd810c13a6db3bb3d
Nguyen Khoa Hung <hung1998pro@gmail.com>
2020-09-06 23:17:19 +0700

    Added Gatsby blog code

---
+++ src/pages/my-page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const MyPage = () => (
+  <Layout>
+    <h1>Welcome to my page</h1>
+  </Layout>
+)
+
+export default MyPage
